65d17761d835cfa1788be47baded4297fa01a3e3
Yang Li <yangli.atlanta@gmail.com>
2020-03-12 22:15:05 -0400

    Update welcome.php

--- 1/welcome.php
+++ 1/welcome.php
@@ -20,9 +20,12 @@ if (isset($_POST['abandon'])) {
 <html>
 
 <body>
+
     <?php
     echo "Welcome " . $_SESSION['email'];
     ?>
+    <p id="countDowntimer"></p>
+
     <form method="post" class="formLayout">
         <div class="formGroup">
             <input type="hidden" name="abandon" value="true">
@@ -30,6 +33,42 @@ if (isset($_POST['abandon'])) {
             <button type="submit">Logout</button>
         </div>
     </form>
+    <script>
+    var countDownDate = new Date("May 5, 2020 12:45:00").getTime();
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById("countDowntimer").innerHTML = days + "d " + hours + "h " +
+            minutes + "m " + seconds + "s ";
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countDowntimer").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+    </script>
+
+
+
+
+
+
+
+
+
 </body>
 
 </html>
