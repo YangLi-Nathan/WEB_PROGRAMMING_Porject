597b4f9e462bf28a81dd59aa4058e5f0fbe7e392
Yang Li <yangli.atlanta@gmail.com>
2020-03-14 12:58:35 -0400

    Update welcome.php

--- 1/welcome.php
+++ 1/welcome.php
@@ -49,7 +49,6 @@ if (isset($_POST['abandon'])) {
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Output the result in an element with id="demo"
         document.getElementById("countDowntimer").innerHTML = days + "d " + hours + "h " +
             minutes + "m " + seconds + "s ";
 
